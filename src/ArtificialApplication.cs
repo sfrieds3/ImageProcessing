52dbdbb2d7ecf767d535f83579ed43ede2aeca7a
thisnullptr <s@thisnullptr.com>
2019-06-05 23:01:40 -0500

    start of reading images

--- src/ArtificialApplication.cs
+++ src/ArtificialApplication.cs
@@ -7,6 +7,8 @@ namespace ArtificialApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ShapeRecognition.ReadImage();
         }
     }
 }
--- src/ShapeRecognition.cs
+++ src/ShapeRecognition.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Text;
 
-namespace ArtificialApplication.src
+namespace ArtificialApplication
 {
     class ShapeRecognition
     {
-
         ShapeRecognition()
         {
 
@@ -15,6 +16,20 @@ namespace ArtificialApplication.src
         ~ShapeRecognition()
         {
 
+        }
+
+        public static void ReadImage()
+        {
+            Bitmap bitmap = new Bitmap(@"C:\dev\AI\assets\square\BlackSquare.png");
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    Console.WriteLine("color: {0}", color);
+                }
+            }
         }
     }
 }
