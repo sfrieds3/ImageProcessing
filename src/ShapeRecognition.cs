3a2b58c31191ea24d537976b5d25e869880b4c60
thisnullptr <s@thisnullptr.com>
2019-06-05 22:44:10 -0500

    add shape recognition initial class file

---
+++ src/ShapeRecognition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtificialApplication.src
+{
+    class ShapeRecognition
+    {
+
+        ShapeRecognition()
+        {
+
+        }
+
+        ~ShapeRecognition()
+        {
+
+        }
+    }
+}
